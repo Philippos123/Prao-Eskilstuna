79f21a87a8e2f4d85dc1a08b5b702c51e9aab126
Philip Samaras <philipsamaras@Philips-MacBook-Air.local>
2024-11-18 14:46:21 +0100

    Hello world

--- project/praktik/admin.py
+++ project/praktik/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PraoAnnons
+
+class PraoAnnonsAdmin(admin.ModelAdmin):
+    list_display = ('rubrik', 'företag', 'publicerad_datum', 'användare')  # Anpassa fälten som ska visas
+    search_fields = ('rubrik', 'företag')  # Sökfält i admin
+
+admin.site.register(PraoAnnons, PraoAnnonsAdmin)
--- project/praktik/forms.py
+++ project/praktik/forms.py
@@ -6,5 +6,11 @@ class PraoAnnonsForm(forms.ModelForm):
         model = PraoAnnons
         fields = ['företag', 'rubrik', 'beskrivning', 'kontaktperson', 'email', 'telefon', 'bild']
         widgets = {
-            'beskrivning': forms.Textarea(attrs={'rows': 4}),
+            'företag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ange företagsnamn'}),
+            'rubrik': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ange annonsrubrik'}),
+            'beskrivning': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Beskriv prao-möjligheten', 'rows': 4}),
+            'kontaktperson': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ange kontaktperson'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'kontakt@foretag.se'}),
+            'telefon': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ange telefonnummer'}),
+            'bild': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
         }
