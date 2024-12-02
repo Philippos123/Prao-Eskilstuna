6638355b021e95e4feb822a5ceb02ef97ede1109
Philip Samaras <philipsamaras@Philips-MacBook-Air.local>
2024-12-02 13:44:13 +0100

    Fix project structure

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+# Sätt miljövariabeln till den rätta sökvägen för dina inställningar
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.project.settings')
 
 application = get_wsgi_application()
