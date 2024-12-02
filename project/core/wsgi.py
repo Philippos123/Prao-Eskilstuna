25936d46e5fccaace01c046b77f3796e781d3672
Philip Samaras <philipsamaras@Philips-MacBook-Air.local>
2024-12-02 14:25:54 +0100

    Delete wsgi part

--- project/core/wsgi.py
+++ project/core/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
 # Ställ in rätt miljövariabel för inställningarna
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.core.settings')
